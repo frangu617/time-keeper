177014139b486919d16393bf9918f31fd26078ba
frangu617 <142462584+frangu617@users.noreply.github.com>
2024-03-01 09:30:08 -0800

    done for now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { TextField, Button, Table, TableBody, TableCell, TableContainer, TableHe
 import DeleteIcon from '@mui/icons-material/Delete';
 // import axios from 'axios';
 import './App.css';
+import {format} from 'date-fns'
 
 
 function App() {
@@ -30,7 +31,7 @@ function App() {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      setLogs(data);
+      setLogs(data.map(log => ({ ...log, time: format(new Date(log.time), 'MM/dd/yyyy hh:mm:ss a') }))); // Format time using date-fns
       setLoading(false); // Set loading to false when data is fetched
     } catch (error) {
       console.error('Error fetching work logs:', error);
